6e7c9b91d4b2afa45864b1571e8d35e85c9e956a
Samara <Samara@DESKTOP-R1UELAH>
2023-03-08 12:59:55 -0300

    Adicionar arquivos de projeto.

---
+++ numeroVitorias/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroVitorias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escreva seu numero de vitorias");
+            int vitorias = int.Parse(Console.ReadLine());
+            Console.WriteLine("Você possue " + vitorias + " Vitorias");
+            Console.WriteLine("Escreva seu numero de derrotas");
+            int derrotas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Você possue " + derrotas + " Derrotas");
+
+            int pontos = vitorias - derrotas;
+            Console.WriteLine("Você possue um total de " + pontos + " pontos.");
+
+            if (pontos >= 10)
+            {
+                Console.WriteLine("Seu time está bem");
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine("Seu time esta mal.");
+                Console.ReadLine();
+            }
+          
+            
+        }
+        
+
+    }
+}
